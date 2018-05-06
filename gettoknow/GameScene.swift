ff1c431aa8df2c0e1a5f00becab1e265602b0b06
Pål Joakimsen <palj@knowit.no>
2018-05-07 00:24:16 +0200

    Creating scene 2

--- gettoknow/GameScene.swift
+++ gettoknow/GameScene.swift
@@ -15,46 +15,28 @@ class GameScene: SKScene {
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-    
+
     }
 
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+       
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+  
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+   
+        let sceneTwo = SceneTwo(fileNamed: "SceneTwo")
+        sceneTwo?.scaleMode = .aspectFill
+        self.view?.presentScene(sceneTwo!, transition: SKTransition.fade(withDuration: 0.5))
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
---
+++ gettoknow/SceneTwo.swift
@@ -0,0 +1,25 @@
+//
+//  SceneTwo.swift
+//  gettoknow
+//
+//  Created by Pål Joakimsen on 06.05.2018.
+//  Copyright © 2018 Pål Joakimsen. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class SceneTwo: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+      
+       
+        // let sceneOne = GameScene(fileNamed: "GameScene")
+        // sceneOne?.scaleMode = .aspectFill
+        // self.view?.presentScene(sceneOne!, transition: SKTransition.fade(withDuration: 0.5))
+    }
+    
+}
